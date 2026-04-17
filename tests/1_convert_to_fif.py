289279d5f1a74de53f9ae6818f02c293775d86fa
dan1kali <171833924+dan1kali@users.noreply.github.com>
2026-04-17 16:05:26 +0200

    add: anonymization steps to pipeline

--- tests/1_convert_to_fif.py
+++ tests/1_convert_to_fif.py
@@ -1,58 +1,86 @@
 # %%
+import project.config as config
 import os
 import mne
-from longitudinal_tempo_waves.initialize import ObtainEEGFilePaths, TRCtoFIF, removeBadChannels, uniqueChannels
+from longitudinal_tempo_waves.initialize import ObtainEEGFilePaths, TRCtoFIF, uniqueChannels, removeBadChannels
+from longitudinal_tempo_waves.anonymize import exportTimeMapping, visualizeRecordingTimeline, anonymizeFifFiles
 import warnings
-import matplotlib.pyplot as plt
-from longitudinal_tempo_waves.mixed_micromed_io.mixed_to_mne import create_mne_from_mixed_micromed_recording
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 mne.set_log_level("ERROR")
 
-################ Specify files and obtain filepaths ################
+BasePath = config.raw_dir
+trcBasePath = os.path.join(BasePath, 'trc')
+fifFilePath_preAnonymize = os.path.join(BasePath, 'fif_rmchans_NOT_anon')
+outputTimeMappingCsv = os.path.join(BasePath, "patientTimeMapping.csv")
 
-basePath = '/Volumes/duguelab_general/DugueLab_Research/Current_Projects/DL_Long_Tempo_Waves/Experiments/Experiment/Data/Raw/trc'
-eegFileList, patientList = ObtainEEGFilePaths(basePath, patient_index=None, file_index=None)
+
+# %%
+
+# (1) ################### CONVERT FROM TRC TO FIF ####################
+
+# -- Specify files and obtain filepaths --
+eegFileList, patientList = ObtainEEGFilePaths(trcBasePath, patient_index=None, file_index=None)
 
 print(f'\n# TRC files selected for conversion - {len(eegFileList)}','\n')
 print(f'\n# Total Patients - {len(patientList)}','\n')
 
-# ####################### Save converted files #######################
-
-# Saves in a folder called "fif" in the same parent directory containing raw file folder
-fifFileList = [os.path.splitext(os.sep.join(["fif" if p == "trc" else p for p in path.split(os.sep)]))[0] + "_raw.fif" 
+# -- Save converted files --
+fifFileList = [os.path.splitext(os.sep.join(["fif_rmchans_NOT_anon" if p == "trc" else p for p in path.split(os.sep)]))[0] + "_raw.fif" 
                                              for path in eegFileList]
 
-channelsToRemove = ['PULS+-PULS-', 'SpO2+-SpO2-', 'BEAT+-BEAT-',
-                    'thor+-thor-', 'abdo+-abdo-', 'xyz+-xyz-','MKR+-MKR-']
-prefixesToRemove = ['el','PNG','dc','SLI','emg']
-suffixesToRemove = ['-0','-1']
+TRCtoFIF(eegFileList, fifFileList, 
+         saveFIF=True, anonymize=False, removeChannels=True, 
+         channelsToRemove=config.chans_to_remove, 
+         prefixesToRemove=config.prefixes_to_remove, 
+         suffixesToRemove=config.suffixes_to_remove)
 
-TRCtoFIF(eegFileList, fifFileList, saveFIF=True, anonymize=True, removeChannels=True, 
-         channelsToRemove=channelsToRemove, prefixesToRemove=prefixesToRemove, suffixesToRemove=suffixesToRemove)
+
+# %%
+
+# (2) ####################### Save time mapping ########################
+
+# -- Save CSV --
+fifFileList_toAnonymize, _ = ObtainEEGFilePaths(fifFilePath_preAnonymize, patient_index=None, file_index=None)
+exportTimeMapping(fifFileList_toAnonymize, outputPath=outputTimeMappingCsv)
+
+# -- Visualize (optional) --
+timeMappingData_csv = os.path.join(BasePath, '___.csv')
+visualizeRecordingTimeline(timeMappingData_csv, patient=None)
+
+
+# %%
+
+# (3) ########################## Bulk anonymize ##########################
+
+anonymizeFifFiles(fifFileList_toAnonymize, overwrite=False)
+
+
+
 
 
 # %%
 
 ################# Construct channel list for removal #################
 
-# basePath = '/Volumes/duguelab_general/DugueLab_Research/Current_Projects/DL_Long_Tempo_Waves/Experiments/Experiment/Data/Raw/fif'
-# patient_index=None
-# fifFilesToExamine,patientList = ObtainEEGFilePaths(basePath, patient_index,file_index=1)
+# Examine channel list --> determine which remove --> fill in config.py
 
-# # Examine channel list
-# # print(f'\n# Patients selected - {len(patientList)}','\n')
-# # unique_chans = uniqueChannels(fifFilesToExamine)
-# # print(f"\nUnique channels across all files:\n{unique_chans}\n")
+# fifBasePath = os.path.join(rawBasePath, 'fif')
+# fifFilesToExamine,patientList = ObtainEEGFilePaths(fifBasePath, patient_index=None,file_index=1)
 
-# Determine channels to remove based on above
-# channelsToRemove = ['PULS+-PULS-', 'SpO2+-SpO2-', 'BEAT+-BEAT-',
-#                     'thor+-thor-', 'abdo+-abdo-', 'xyz+-xyz-','MKR+-MKR-']
-# prefixesToRemove = ['el','PNG','dc','SLI','emg','ecg']
-# suffixesToRemove = ['-0','-1']
+# unique_chans = uniqueChannels(fifFilesToExamine)
+
+# print(f'\n# Patients selected - {len(patientList)}','\n')
+# print(f"\nUnique channels across all files:\n{unique_chans}\n")
+
 
 # %%
 # ##################### Bulk Remove bad channels #####################
 
-# basePath = '/Volumes/duguelab_general/DugueLab_Research/Current_Projects/DL_Long_Tempo_Waves/Experiments/Experiment/Data/Raw/fif'
-# fifFilesToExamine,patientList = ObtainEEGFilePaths(basePath)
-# removeBadChannels(fifFilesToExamine, channelsToRemove, prefixesToRemove, suffixesToRemove, replaceCleaned=False)
+# fifBasePath = os.path.join(rawBasePath, 'fif')
+# fifFilesToExamine,patientList = ObtainEEGFilePaths(fifBasePath, patient_index=None,file_index=None)
+
+# removeBadChannels(fifFilesToExamine, 
+#                   channelsToRemove=config.chans_to_remove, 
+#                   prefixesToRemove=config.prefixes_to_remove, 
+#                   suffixesToRemove=config.suffixes_to_remove, 
+#                   replaceCleaned=False)
