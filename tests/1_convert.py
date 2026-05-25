d72320b82ea48945f5ca3809388d75f5a99bdc17
dan1kali <171833924+dan1kali@users.noreply.github.com>
2026-05-25 17:58:22 +0200

    chore: renaming, styling

--- tests/1_convert.py
+++ tests/1_convert.py
@@ -9,24 +9,24 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 mne.set_log_level("ERROR")
 
 BasePath = config.raw_dir
-trcBasePath = os.path.join(BasePath, 'trc')
 fifFilePath_preAnonymize = os.path.join(BasePath, 'fif_rmchans_NOT_anon')
 outputTimeMappingCsv = os.path.join(BasePath, "patientTimeMapping.csv")
 
 
 # %%
 
-# (1) ################### CONVERT FROM TRC TO FIF ####################
+############################################################################
+######################## Convert from .trc to .fif #########################
+############################################################################
 
-# -- Specify files and obtain filepaths --
-eegFileList, patientList = ObtainEEGFilePaths(trcBasePath, patient_index=None, file_index=None)
+# ------------------ Specify files and obtain filepaths -------------------
+eegFileList, patientList = ObtainEEGFilePaths(config.trc_raw_dir, patient_index=None, file_index=None)
 
 print(f'\n# TRC files selected for conversion - {len(eegFileList)}','\n')
 print(f'\n# Total Patients - {len(patientList)}','\n')
 
-# -- Save converted files --
-fifFileList = [os.path.splitext(os.sep.join(["fif_rmchans_NOT_anon" if p == "trc" else p for p in path.split(os.sep)]))[0] + "_raw.fif" 
-                                             for path in eegFileList]
+# ------------------------ Save converted files -------------------------
+fifFileList = [os.path.splitext(os.sep.join(["fif_rmchans_NOT_anon" if p == "trc" else p for p in path.split(os.sep)]))[0] + "_raw.fif" for path in eegFileList]
 
 TRCtoFIF(eegFileList, fifFileList, 
          saveFIF=True, anonymize=False, removeChannels=True, 
@@ -37,30 +37,9 @@ TRCtoFIF(eegFileList, fifFileList,
 
 # %%
 
-# (2) ####################### Save time mapping ########################
-
-# -- Save CSV --
-fifFileList_toAnonymize, _ = ObtainEEGFilePaths(fifFilePath_preAnonymize, patient_index=None, file_index=None)
-exportTimeMapping(fifFileList_toAnonymize, outputPath=outputTimeMappingCsv)
-
-# -- Visualize (optional) --
-timeMappingData_csv = os.path.join(BasePath, '___.csv')
-visualizeRecordingTimeline(timeMappingData_csv, patient=None)
-
-
-# %%
-
-# (3) ########################## Bulk anonymize ##########################
-
-anonymizeFifFiles(fifFileList_toAnonymize, overwrite=False)
-
-
-
-
-
-# %%
-
-################# Construct channel list for removal #################
+##########################################################################
+################### Construct channel list for removal ###################
+##########################################################################
 
 # Examine channel list --> determine which remove --> fill in config.py
 
@@ -74,7 +53,9 @@ anonymizeFifFiles(fifFileList_toAnonymize, overwrite=False)
 
 
 # %%
-# ##################### Bulk Remove bad channels #####################
+##########################################################################
+######################## Bulk Remove bad channels ########################
+##########################################################################
 
 # fifBasePath = os.path.join(rawBasePath, 'fif')
 # fifFilesToExamine,patientList = ObtainEEGFilePaths(fifBasePath, patient_index=None,file_index=None)
---
+++ tests/2_annotate.py
@@ -0,0 +1,123 @@
+# %%
+import json
+import os
+import mne
+import project.config as config
+from longitudinal_tempo_waves.initialize import ObtainEEGFilePaths
+from longitudinal_tempo_waves.annotate import cleanFifAnnotations, compileAnnotationsFromFilelist, loadAnnotationsFromFile, relabelFifAnnotations, visualizeAnnotationTimeline
+import pandas as pd
+
+# BasePath = config.raw_dir
+BasePath = '/Users/macbook/Work/DugueLab/Longitudinal_Tempo_Waves/Data/long_duration'
+
+
+# %%
+#########################################################################
+################ Clean annotations and resave .fif files ################
+#########################################################################
+
+# ----------------- Obtain FIF file paths -----------------
+fifBasePath = os.path.join(BasePath, 'fif')
+fifFileList, _ = ObtainEEGFilePaths(fifBasePath,patient_index=None)
+
+# ------------- Get rid of unicode in annotations -------------
+fifSaveFileList = [os.path.splitext(os.sep.join(["fif_fix_annotations" if p == "fif" else p for p in path.split(os.sep)]))[0] + ".fif" for path in fifFileList]
+cleanFifAnnotations(fifFileList, savePathList=fifSaveFileList)
+
+
+# %%
+
+########################################################################
+################# Export annotations from .fif files ###################
+########################################################################
+
+# -------------------- Build load and save paths -----------------------
+fifBasePath = os.path.join(BasePath, 'fif_fix_annotations')
+fifFileList, _ = ObtainEEGFilePaths(fifBasePath,patient_index=None)
+
+parquetPath = os.path.join(BasePath, "all_annotations.parquet")
+csvPath = os.path.join(BasePath, "all_annotations.csv")
+
+savePath=parquetPath
+
+# ------- Compile all annotations and export to .csv/.parquet ----------
+annotationDF = compileAnnotationsFromFilelist(fifFileList, savePath)
+
+
+# %%
+
+########################################################################
+############## (optional) Visualize Annotation Timeline ################
+########################################################################
+
+patient_ID = [1]
+filename_list = None
+visualizeAnnotationTimeline(parquetPath,patient_ID=patient_ID,filename_list=filename_list)
+
+
+# %%
+
+########################################################################
+#################### Examine unique annotations ########################
+########################################################################
+
+# --------------------- Build save paths ----------------------
+annotationTxt = os.path.join(BasePath, "annotations_unique.txt")
+annotationTableParquet = os.path.join(BasePath, "annotations_toreview.parquet")
+annotationTableCsv = os.path.join(BasePath, "annotations_toreview.csv")
+txtSavePath = annotationTxt
+tableSavePath = annotationTableParquet
+
+# --------------------- Load the .csv/.parquet ----------------------
+annotationDF = loadAnnotationsFromFile(parquetPath)
+
+# --------------------- Get unique annotations ----------------------
+uniqueAnnotations = sorted(annotationDF["annotation"].unique())
+# print(uniqueAnnotations)
+
+# ------------------ Save unique annotations to .txt -----------------
+# with open(txtSavePath, "w", encoding="utf-8") as f:
+#     for ann in uniqueAnnotations:
+#         f.write(str(ann) + "\n")
+
+# ------------- Save unique annotations to editable table ------------
+review_df = pd.DataFrame({"old_label": uniqueAnnotations, "new_label": uniqueAnnotations }) 
+review_df.to_parquet(tableSavePath, engine="pyarrow")
+# review_df.to_csv(tableSavePath, index=False)
+
+
+# %%
+
+########################################################################
+############# Create rename dictionary from edited table ###############
+########################################################################
+
+# --------------------- Load file paths ----------------------
+reviewLoadPath = annotationTableParquet
+
+# review_df = pd.read_csv(reviewLoadPath)
+review_df = pd.read_parquet(annotationTableParquet)
+
+# ---------------- Create dictionary to .json ------------------
+renameDict = dict(zip(review_df["old_label"], review_df["new_label"]))
+
+config_path = "/src/project/config.json"
+renameConfig = {"rename_dict": renameDict}
+with open(config_path, "w") as f:
+    json.dump(renameConfig, f, indent=2)
+
+
+# %%
+########################################################################
+################ Rename annotations and save new files #################
+########################################################################
+
+# ---------------- Load rename dictionary from .json ------------------
+
+with open(config_path, "r") as f:
+    config = json.load(f)
+renameDict = config["rename_dict"]
+
+# ---------------- Relabel annotations ------------------
+relabelFifAnnotations(fifFileList, renameDict, overwrite=False)
+
